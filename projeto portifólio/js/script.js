d310cfea226bf2b73968141a0a95e8f5cf2da5e0
DiegoHessel <diego.verdi@sptech.school>
2023-12-05 12:13:50 -0300

    mini projeto

---
+++ projeto portifólio/js/script.js
@@ -0,0 +1,45 @@
+function sobre() {
+    window.location.href = "sobre.html";
+}
+    function voltar(){
+        window.location.href = "participante.html";
+    }
+    function sandile1(){
+        window.location.href = "./fotos/Bradesco.pdf";
+        }
+        function sandile2(){
+        window.location.href = "./fotos/Embalar sketch.pdf";
+        }
+        function sandile3(){
+        window.location.href = "./fotos/Revista Oculto.pdf";
+        }
+    
+        function matheus1(){
+        window.location.href = "./fotos/capa do livro.jpg";
+        }
+        function matheus2(){
+        window.location.href = "./fotos/Luffy and nami.jpg";
+        }
+        function matheus3(){
+        window.location.href = "./fotos/utopia2.pdf";
+        }
+       
+        function rapha1(){
+        window.location.href = "./fotos/FolderFinal.jpg";
+        }
+        function rapha2(){
+        window.location.href = "./fotos/Copos Uliving.jpg";
+        }
+        function rapha3(){
+        window.location.href = "./fotos/DioramaJardim.png";
+        }
+        
+        function isabela1(){
+        window.location.href = "./fotos/PORTFÍLIO.pdf";
+        }
+        function isabela2(){
+        window.location.href = "./fotos/chamytoFinal.pdf";
+        }
+        function isabela3(){
+        window.location.href = "./fotos/DesignSemNome.pdf";
+        }
